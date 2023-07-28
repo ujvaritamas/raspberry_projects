849473a421f7d6713280549d970ee005467f96e1
ujvaritamas <49878381+ujvaritamas@users.noreply.github.com>
2023-07-28 10:47:17 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # raspberry_projects
 basic raspberry control
+
+Install wiringPi
+    gpio -v
+    gpio readall
+    sudo apt-get update
+    sudo apt-get upgrade
+    git clone git://git.drogon.net/wiringPi
+
+    cd /<path>/wiringPi
+    ./build
+
+    (GPIO numbering: https://www.aranacorp.com/en/program-your-raspberry-pi-with-c/)
+
+    find pinout with this command:
+    gpio readall
+
+    gcc test.cc -o test -lwiringPi
---
+++ gpio_ctrl/test.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <wiringPi.h>
+
+using namespace std;
+
+#define RED_LED_PIN 29
+
+void setup(){
+
+	pinMode(RED_LED_PIN, OUTPUT);
+	cout<<"Ledpin setupt finished" << endl;
+}
+
+int main(void){
+
+    if(wiringPiSetup()<0){
+		cout<<"setup wiring pi failed"<<endl;
+		return 1;
+	}
+    
+    setup();
+    // Main program loop
+    while(1)
+    {
+        // Toggle the LED
+        digitalWrite(0, HIGH);
+        delay(500); 	// Delay 500ms
+        digitalWrite(0, LOW);
+        delay(500); 	// Delay 500ms
+    {
+
+
+    return 0;
+}
